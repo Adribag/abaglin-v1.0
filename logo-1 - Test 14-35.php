d6c993ddd12cff31f4e599ec8f3d168436e96508
Adribag <adrien.baglin@gmail.com>
2016-03-29 15:04:40 +0200

    premier ajout

---
+++ logo-1 - Test 14-35.php
@@ -0,0 +1,16 @@
+<?php
+include_once "header.php";
+?>
+
+    <h2>LOGO</h2>
+    <figure>
+        <img class="intro" src="img/logo.png"/>
+        <figcaption>
+            Description du logo
+        </figcaption>
+    </figure>
+    <a class="back" href="logo.php">Retour aux logos</a>
+
+<?php
+include_once "footer.php";
+?>
